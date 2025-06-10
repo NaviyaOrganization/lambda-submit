06be33da92dcb51a1d95ad3729e09037736e76a4
lixxce5017 <lixxce5017@gmail.com>
2025-06-11 07:13:11 +0900

    Initial commit: Setup project structure for submit lambda

---
+++ src/main/java/com/naviya/lamda/NaviyaLambdaSubmitApplication.java
@@ -0,0 +1,13 @@
+package com.naviya.lamda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaviyaLambdaSubmitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaviyaLambdaSubmitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naviya/lamda/NaviyaLambdaSubmitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naviya.lamda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaviyaLambdaSubmitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
